a71fe122e4fee1b926d549688900cbedd528d262
Metin Çetin <110323658+metinncetinn@users.noreply.github.com>
2024-10-16 15:26:40 +0300

    Add files via upload

---
+++ CodeSpace/calis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Gazebo terminali
+gnome-terminal -- bash -c "cd && gz sim -v4 -r iris_runway.sdf; exec bash"
+sleep 10  # Gazebo'ya 5 saniye bekle
+
+# ArduPilot terminali
+gnome-terminal -- bash -c "cd ~/ardupilot/Tools/autotest && python sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console; exec bash"
