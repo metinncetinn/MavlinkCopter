78e13b0b92edd0909e37de3f1433cc36ae38312f
Metin <metinncetinn@outlook.com>
2024-10-17 11:27:36 +0300

    Hava Durumu Kontrol

--- CodeSpace/HavaSartlari.py
+++ CodeSpace/HavaSartlari.py
@@ -1,8 +1,28 @@
 import requests
+from datetime import datetime
+import key
 
-api_key = "api"
-city = "London"
+api_key = key.api_key
+city = "Konya"
+
+# API çağrısını yap
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 response = requests.get(url)
 weather_data = response.json()
-print(weather_data)
+
+# Sıcaklığı C cinsine çevir
+kelvin_temp = weather_data['main']['temp']
+celsius_temp = kelvin_temp - 273.15
+
+# Gün doğumu ve gün batımı zamanlarını al ve dönüştür
+sunrise_time = datetime.fromtimestamp(weather_data['sys']['sunrise'])
+sunset_time = datetime.fromtimestamp(weather_data['sys']['sunset'])
+
+#Rüzgar hızı
+windSpeed = weather_data['wind']['speed'] * 3.6
+
+# Sonuçları yazdır
+print(f"Sicaklik: {celsius_temp:.2f} °C")
+print(f"Rüzgar Hizi: {windSpeed:.2f} km/h")
+print(f"Gün doğumu: {sunrise_time.strftime('%H:%M:%S')}")
+print(f"Gün batimi: {sunset_time.strftime('%H:%M:%S')}")
---
+++ CodeSpace/key.py
@@ -0,0 +1,1 @@
+api_key= "8c6424ca2c1cc7eb962af9c2a814c38b"
