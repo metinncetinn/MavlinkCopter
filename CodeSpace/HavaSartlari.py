409e052268f36ad090e3fd48e89703aa1da6906d
Metin <metinncetinn@outlook.com>
2024-10-17 10:47:35 +0300

    api

--- CodeSpace/HavaSartlari.py
+++ CodeSpace/HavaSartlari.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "99c3a681bc8bc5a9f7cb26db1f21b18e"
+api_key = "api"
 city = "London"
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 response = requests.get(url)
